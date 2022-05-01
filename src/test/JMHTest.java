0ceda5a28b6c91e71e566affac976012128a0a3c
Thiago Vinícius <thiagovao2000@hotmail.com>
2022-05-01 20:34:34 -0300

    Configure jmh test setup

--- src/test/JMHTest.java
+++ src/test/JMHTest.java
@@ -12,9 +12,9 @@ public class JMHTest {
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
 				.include(KnnBenchmark.class.getSimpleName())
-				.warmupIterations(2)
+				.warmupIterations(3)
 				.shouldDoGC(true)
-				.measurementIterations(2).forks(1)
+				.measurementIterations(5).forks(1)
 				.addProfiler(GCProfiler.class)
 				.addProfiler(StackProfiler.class)
 				.jvmArgs("-server", "-Xms2048m", "-Xmx2048m").build();
--- src/test/KnnBenchmark.java
+++ src/test/KnnBenchmark.java
@@ -24,8 +24,8 @@ import serial.KNearestNeighbors;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
-@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @Fork(1)
 public class KnnBenchmark {
