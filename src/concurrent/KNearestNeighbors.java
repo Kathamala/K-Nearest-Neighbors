406762d5f5cff29d73f623b2f16bf2be58e7d201
Thiago Vinícius <thiagovao2000@hotmail.com>
2022-06-29 23:20:00 -0300

    Implements concurrent list

--- src/concurrent/KNearestNeighbors.java
+++ src/concurrent/KNearestNeighbors.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -22,13 +23,14 @@ public class KNearestNeighbors {
 	
 	public ArrayList<ArrayList<Float>> data;
 	public ArrayList<Float> newData = new ArrayList<Float>();
-	public AtomicReferenceArray<Item> distances;
+	//public AtomicReferenceArray<Item> distances;
+	public List<Item> distances = Collections.synchronizedList(new ArrayList<Item>());
 		
 	public ArrayList<ThreadUnity> threads = new ArrayList<ThreadUnity>();
 
 	public void startKnn(ArrayList<Float> _newData, ArrayList<ArrayList<Float>> _data) throws IOException, InterruptedException {
 		data = _data;
-		distances = new AtomicReferenceArray<Item>(data.size());
+		//distances = new AtomicReferenceArray<Item>(data.size());
 		newData = _newData;
 		
 		ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS); 
@@ -54,37 +56,25 @@ public class KNearestNeighbors {
 		return count.getAndIncrement();
 	}
 	
-	public Item addDistance(int position, Item item) {
-		distances.set(position, item);
+	public Item addDistance(/*int position,*/ Item item) {
+		//distances.set(position, item);
+		distances.add(item);
 		return item;
 	}	
 	
-	private Float findClass() {		
+	private Float findClass() {	
+		/*
 		ArrayList<Item> distancesAL = new ArrayList<Item>();
 		
-		for(int i=0; i<distances.length(); i++) {
+		for(int i=0; i<distances.size(); i++) {
 			if(distances.get(i) != null) distancesAL.add(distances.get(i));
-		}
-		
-		/*
-		distancesAL.sort(new ItemComparator());
-		
-		List<Float> classes = new ArrayList<Float>();
-			
-		for(int i=0; i<k; i++) {
-			classes.add(distancesAL.get(i).classValue);
 		}
 		*/
 		
 		List<Float> classes = new ArrayList<Float>();
 		
-		distancesAL.parallelStream().sorted(new ItemComparator()).limit(k).forEach(e -> {classes.add(e.classValue);});
-		
-		
-		
-		
-		//Float mostCommonElement = mostCommon(classes);
-		
+		distances.parallelStream().sorted(new ItemComparator()).limit(k).forEach(e -> {classes.add(e.classValue);});
+
 		Float mostCommonElement = classes.parallelStream()
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
 				.entrySet()
@@ -97,40 +87,4 @@ public class KNearestNeighbors {
 		System.out.println("The new item belongs to the class " + mostCommonElement + ", with a k=" + k + ".");
 		return mostCommonElement;
 	}
-
-	/*
-	private Float mostCommon(List<Float> values) {
-		if(values == null || values.size() == 0) {
-			return 0f;
-		}
-		
-		Collections.sort(values);		
-		
-		Float res = values.get(0);
-		int max_count = 1;
-        int curr_count = 1;
-		
-        for (int i = 1; i < values.size(); i++)
-        {
-            if (values.get(i).equals(values.get(i-1)))
-                curr_count++;
-            else
-            {
-                if (curr_count > max_count)
-                {
-                    max_count = curr_count;
-                    res = values.get(i-1);
-                }
-                curr_count = 1;
-            }
-        }   
-        
-        if (curr_count > max_count)
-        {
-            max_count = curr_count;
-            res = values.get(values.size()-1);
-        }        
-		
-        return res;
-	}	*/	
 }
